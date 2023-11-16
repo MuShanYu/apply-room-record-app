9f25b0cbf4be3793f02cc18b212038d98ec30ec0
yulf <yulf@startimes.com.cn>
2023-11-16 17:36:49 +0800

    完善底部自定义导航栏

--- pages/index/component/home.vue
+++ pages/index/component/home.vue
@@ -1,19 +1,11 @@
 <template>
 	<view>
 		<!-- 顶部自定义导航 -->
-		<tn-nav-bar>自定义导航栏</tn-nav-bar>
+		<tn-nav-bar :isBack='false'>首页</tn-nav-bar>
 		<!-- 页面内容 -->
 		<view :style="{paddingTop: vuex_custom_bar_height + 'px'}">
-			<tn-button @click='test()' backgroundColor="#01BEFF" :plain="false">
-				点击进行登录
-			</tn-button>
-			<view class="">
-				<tn-button @click='test1()' backgroundColor="#01BEFF" :plain="false">
-					测试
-				</tn-button>
-			</view>
+			
 		</view>
-
 	</view>
 </template>
 
@@ -22,12 +14,13 @@
 	export default {
 		data() {
 			return {
-
+				title: 'aaaaa',
+				show: true
 			}
 		},
 		methods: {
 			test() {
-				this.$Router.push('/pages/public/login')
+				this.show = !this.show
 			},
 			test1() {
 				recordApi.userAccessRecordListApi(1, 20)
