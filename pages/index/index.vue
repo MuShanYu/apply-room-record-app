a38228fee8afc0ceb4d998966b3851ac71efc0f5
yulf <yulf@startimes.com.cn>
2023-11-16 17:36:49 +0800

    完善底部自定义导航栏

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,31 +1,40 @@
 <template>
-	<view>
+	<view class="index">
+
 		<!-- 二级页面 -->
 		<view v-if="tabberPageLoadFlag[0]" :style="{
-		    display: currentTabbarIndex === 0 ? '' : 'none'
-		  }">
+		  display: currentTabbarIndex === 0 ? '' : 'none'
+		}">
 			<scroll-view class="custom-tabbar-page" scroll-y enable-back-to-top @scrolltolower="tabbarPageScrollLower">
 				<home ref="home"></home>
 			</scroll-view>
 		</view>
 
 		<view v-if="tabberPageLoadFlag[1]" :style="{
-		    display: currentTabbarIndex === 1 ? '' : 'none'
-		  }">
+		  display: currentTabbarIndex === 1 ? '' : 'none'
+		}">
 			<scroll-view class="custom-tabbar-page" scroll-y enable-back-to-top @scrolltolower="tabbarPageScrollLower">
 				<work ref="work"></work>
 			</scroll-view>
 		</view>
 
 		<view v-if="tabberPageLoadFlag[2]" :style="{
-		    display: currentTabbarIndex === 2 ? '' : 'none'
-		  }">
+		  display: currentTabbarIndex === 2 ? '' : 'none'
+		}">
 			<scroll-view class="custom-tabbar-page" scroll-y enable-back-to-top @scrolltolower="tabbarPageScrollLower">
 				<profile ref="profile"></profile>
 			</scroll-view>
 		</view>
 
-		<!-- 冒泡特效 -->
+		<!-- 右上角形式的tabbar按钮-->
+		<view class="">
+			<view class="icon15__item--icon tn-flex tn-flex-row-center tn-flex-col-center tn-shadow-blur button-2"
+				@tap.stop="changeTabbar(4)">
+				<view class="tn-icon-ghost-fill"
+					:class="[currentTabbarIndex === 4 ? 'tn-color-blue' : 'tn-color-white']"></view>
+			</view>
+		</view>
+
 		<view class="tabbar tn-bg-white">
 
 			<div class="">
@@ -53,58 +62,64 @@
 				<div class="g-bubble"></div>
 				<div class="g-bubble"></div>
 			</div>
-		</view>
-		<!-- 底部导航栏 -->
-		<view class="action" @tap.stop="changeTabbar(0)">
-			<view class="bar-icon">
-				<image class="" :src="`/static/tabbar/tn-tabbar0${currentTabbarIndex === 0 ? '-curnew' : ''}.png`"></image>
-			</view>
-			<view class="" :class="[currentTabbarIndex === 0 ? 'tn-color-blue' : 'tn-color-gray']">首页</view>
-		</view>
 
-		<view class="action bar-center" @tap.stop="changeTabbar(1)">
-			<view class="nav-index-button">
-				<view class="nav-index-button__content">
-					<view class="nav-index-button__content--icon tn-flex tn-flex-row-center tn-flex-col-center">
-						<view class="bar-circle">
-							<image class=""
-								src='https://cdn.nlark.com/yuque/0/2021/png/280373/1634526816906-assets/web-upload/568740d8-8669-48d5-9b3e-b510e39d127d.png'>
-							</image>
+			<!-- 底部导航栏 -->
+			<view class="action" @tap.stop="changeTabbar(0)">
+				<view class="bar-icon">
+					<image class="" :src="`/static/tabbar/tn-tabbar0${currentTabbarIndex === 0 ? '-curnew' : ''}.png`">
+					</image>
+				</view>
+				<view class="" :class="[currentTabbarIndex === 0 ? 'tn-color-blue' : 'tn-color-gray']">首页</view>
+			</view>
+
+			<view class="action bar-center" @tap.stop="changeTabbar(1)">
+				<view class="nav-index-button">
+					<view class="nav-index-button__content">
+						<view class="nav-index-button__content--icon tn-flex tn-flex-row-center tn-flex-col-center">
+							<view class="bar-circle">
+								<image class=""
+									src='https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/tab/faxian.png'>
+								</image>
+							</view>
+						</view>
+					</view>
+					<!-- 特效 -->
+					<view class="nav-index-button__meteor">
+						<view class="nav-index-button__meteor__wrapper">
+							<view v-for="(item,index) in 6" :key="index" class="nav-index-button__meteor__item"
+								:style="{transform: `rotateX(${-60 + (30 * index)}deg) rotateZ(${-60 + (30 * index)}deg)`}">
+								<view class="nav-index-button__meteor__item--pic"></view>
+							</view>
 						</view>
 					</view>
 				</view>
-				<!-- 特效 -->
-				<view class="nav-index-button__meteor">
-					<view class="nav-index-button__meteor__wrapper">
-						<view v-for="(item,index) in 6" :key="index" class="nav-index-button__meteor__item"
-							:style="{transform: `rotateX(${-60 + (30 * index)}deg) rotateZ(${-60 + (30 * index)}deg)`}">
-							<view class="nav-index-button__meteor__item--pic"></view>
-						</view>
-					</view>
+			</view>
+
+			<view class="action" @tap.stop="changeTabbar(2)">
+				<view class="bar-icon">
+					<image class="" :src="`/static/tabbar/tn-tabbar3${currentTabbarIndex === 2 ? '-curnew' : ''}.png`">
+					</image>
+				</view>
+				<view class="" :class="[currentTabbarIndex === 2 ? 'tn-color-blue' : 'tn-color-gray']">我的</view>
+			</view>
+		</view>
+
+		<view class="tnwave waveAnimation">
+			<view class="waveWrapperInner bgTop">
+				<view class="wave waveTop"
+					style="background-image: url('https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/tab/wave-2.png')">
 				</view>
 			</view>
-		</view>
-
-		<view class="action" @tap.stop="changeTabbar(2)">
-			<view class="bar-icon">
-				<image class="" :src="`/static/tabbar/tn-tabbar3${currentTabbarIndex === 2 ? '-curnew' : ''}.png`"></image>
+			<view class="waveWrapperInner bgMiddle">
+				<view class="wave waveMiddle"
+					style="background-image: url('https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/tab/wave-2.png')">
+				</view>
 			</view>
-			<view class="" :class="[currentTabbarIndex === 2 ? 'tn-color-blue' : 'tn-color-gray']">我的</view>
-		</view>
-	</view>
-
-	<view class="tnwave waveAnimation">
-		<view class="waveWrapperInner bgTop">
-			<view class="wave waveTop" style="background-image: url('https://resource.tuniaokj.com/images/wave/wave-2.png')">
+			<view class="waveWrapperInner bgBottom">
+				<view class="wave waveBottom"
+					style="background-image: url('https://mushanyu-app-arr.oss-cn-beijing.aliyuncs.com/static/tab/wave-1.png')">
+				</view>
 			</view>
-		</view>
-		<view class="waveWrapperInner bgMiddle">
-			<view class="wave waveMiddle"
-				style="background-image: url('https://resource.tuniaokj.com/images/wave/wave-2.png')"></view>
-		</view>
-		<view class="waveWrapperInner bgBottom">
-			<view class="wave waveBottom"
-				style="background-image: url('https://resource.tuniaokj.com/images/wave/wave-1.png')"></view>
 		</view>
 	</view>
 </template>
@@ -115,6 +130,11 @@
 	import Profile from './component/profile.vue'
 
 	export default {
+		components: {
+			Home,
+			Work,
+			Profile
+		},
 		data() {
 			return {
 				currentTabbarIndex: 0,
@@ -123,23 +143,25 @@
 				tabberPageLoadFlag: []
 			}
 		},
-		components: {
-			Home,
-			Work,
-			Profile
-		},
 		onLoad(options) {
 			const index = Number(options.index || 0)
 			// 根据底部tabbar菜单列表设置对应页面的加载情况
-			for (let i = 0; i < 3; i++) {
+			for (let i = 0; i < 5; i++) {
 				this.tabberPageLoadFlag.push(i === index)
 			}
 			this.changeTabbar(index)
 		},
+		onReady() {
+
+		},
 		methods: {
+
+
 			// 导航页面滚动到底部
 			tabbarPageScrollLower(e) {
-
+				if (this.currentTabbarIndex === 1) {
+					this.$refs.pageB.getRandomData && this.$refs.pageB.getRandomData()
+				}
 			},
 
 			// 修改当前选中的tabbar
@@ -162,6 +184,8 @@
 					this.tabberPageLoadFlag[index] = true
 				}
 			},
+
+
 		}
 	}
 </script>
@@ -283,6 +307,266 @@
 			transform: translate(-50%, -180px) scale(0.3);
 		}
 	}
-	
-	
+
+
+
+
+	.index {
+		width: 100%;
+		height: 100vh;
+		position: relative;
+
+		.custom-tabbar-page {
+			width: 100%;
+			// height: calc(100vh - 110rpx);
+			height: 100vh;
+			box-sizing: border-box;
+			padding-bottom: 0rpx;
+			padding-bottom: calc(0rpx + constant(safe-area-inset-bottom));
+			padding-bottom: calc(0rpx + env(safe-area-inset-bottom));
+		}
+
+
+		/* 底部导航 statr */
+		.tabbar {
+			width: 100%;
+			height: 45rpx;
+			height: calc(45rpx + constant(safe-area-inset-bottom));
+			height: calc(45rpx + env(safe-area-inset-bottom));
+			position: fixed;
+			bottom: 0rpx;
+			left: 0;
+			right: 0;
+			background-color: transparent;
+			z-index: 998;
+			padding-bottom: calc(65rpx + constant(safe-area-inset-bottom));
+			padding-bottom: calc(65rpx + env(safe-area-inset-bottom));
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			// box-shadow: 0rpx 0rpx 30rpx 0rpx rgba(0, 0, 0, 0.07);
+
+			.action {
+				font-size: 22rpx;
+				position: relative;
+				flex: 1;
+				text-align: center;
+				padding: 0;
+				display: block;
+				height: auto;
+				line-height: 1;
+				margin: 0;
+				overflow: initial;
+
+				.bar-icon {
+					width: 100rpx;
+					position: relative;
+					display: block;
+					height: auto;
+					margin: 0 auto 10rpx;
+					text-align: center;
+					font-size: 42rpx;
+
+					image {
+						width: 55rpx;
+						height: 55rpx;
+						display: inline-block;
+					}
+				}
+
+				.bar-circle {
+					position: relative;
+					display: block;
+					margin: 0rpx auto 0rpx;
+					text-align: center;
+					font-size: 52rpx;
+					line-height: 90rpx;
+					width: 100rpx !important;
+					height: 100rpx !important;
+					overflow: hidden;
+
+					image {
+						width: 100rpx;
+						height: 100rpx;
+						display: inline-block;
+						margin: 0rpx auto 0rpx;
+					}
+				}
+			}
+
+			.bar-center {
+				animation: suspension 3s ease-in-out infinite;
+
+				.nav-index-button {
+					animation: suspension 3s ease-in-out infinite;
+					z-index: 999999;
+
+
+					&__content {
+						position: absolute;
+						width: 100rpx;
+						height: 100rpx;
+						top: 50%;
+						left: 50%;
+						transform: translate(-50%, -50%);
+
+						&--icon {
+							width: 100rpx;
+							height: 100rpx;
+							font-size: 60rpx;
+							border-radius: 50%;
+							margin-bottom: 18rpx;
+							position: relative;
+							z-index: 1;
+							transform: scale(0.85);
+
+							&::after {
+								content: " ";
+								position: absolute;
+								z-index: -1;
+								width: 100%;
+								height: 100%;
+								left: 0;
+								bottom: 0;
+								border-radius: inherit;
+								opacity: 1;
+								transform: scale(1, 1);
+								background-size: 100% 100%;
+								// background-image: url(https://resource.tuniaokj.com/images/cool_bg_image/icon_bg6.png);
+							}
+						}
+					}
+
+					&__meteor {
+						position: absolute;
+						top: 50%;
+						left: 50%;
+						width: 100rpx;
+						height: 100rpx;
+						transform-style: preserve-3d;
+						transform: translate(-50%, -50%) rotateY(75deg) rotateZ(10deg);
+
+						&__wrapper {
+							width: 100rpx;
+							height: 100rpx;
+							transform-style: preserve-3d;
+							animation: spin 20s linear infinite;
+						}
+
+						&__item {
+							position: absolute;
+							width: 100rpx;
+							height: 100rpx;
+							border-radius: 1000rpx;
+							left: 0;
+							top: 0;
+
+							&--pic {
+								display: block;
+								width: 100%;
+								height: 100%;
+								background: url(https://resource.tuniaokj.com/images/cool_bg_image/arc3.png) no-repeat center center;
+								background-size: 100% 100%;
+								animation: arc 4s linear infinite;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		/* 底部导航 end */
+	}
+
+	@keyframes suspension {
+
+		0%,
+		100% {
+			transform: translateY(0);
+		}
+
+		50% {
+			transform: translateY(-0.6rem);
+		}
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotateX(0deg);
+		}
+
+		100% {
+			transform: rotateX(-360deg);
+		}
+	}
+
+	@keyframes arc {
+		to {
+			transform: rotate(360deg);
+		}
+	}
+
+
+	/* 新*/
+	/* 按钮 */
+	.button-1 {
+		background-color: rgba(0, 0, 0, 0.15);
+		position: fixed;
+		/* bottom:200rpx;
+      right: 20rpx; */
+		bottom: 24%;
+		right: 30rpx;
+		z-index: 1001;
+		border-radius: 100px;
+	}
+
+	.button-2 {
+		background-color: rgba(0, 0, 0, 0.15);
+		position: fixed;
+		/* bottom:200rpx;
+      right: 20rpx; */
+		bottom: 15%;
+		right: 30rpx;
+		z-index: 1001;
+		border-radius: 100px;
+	}
+
+	/* 图标容器15 start */
+	.icon15 {
+		&__item {
+			width: 30%;
+
+			border-radius: 10rpx;
+			padding: 30rpx;
+			margin: 20rpx 10rpx;
+			transform: scale(1);
+			transition: transform 0.3s linear;
+			transform-origin: center center;
+
+			&--icon {
+				width: 100rpx;
+				height: 100rpx;
+				font-size: 50rpx;
+				border-radius: 50%;
+				margin-bottom: 18rpx;
+				z-index: 1;
+
+				&::after {
+					content: " ";
+					position: absolute;
+					z-index: -1;
+					width: 100%;
+					height: 100%;
+					left: 0;
+					bottom: 0;
+					border-radius: inherit;
+					opacity: 1;
+					transform: scale(1, 1);
+					background-size: 100% 100%;
+
+
+				}
+			}
+		}
+	}
 </style>
--- router/index.js
+++ router/index.js
@@ -9,30 +9,31 @@ const router = createRouter({
 });
 //全局路由前置守卫
 router.beforeEach((to, from, next) => {
-	if (process.env.NODE_ENV === 'development') { 
-		console.log('to:' + to.path);
-		console.log('from:' + from.path);
-	}
-	const hasLogin = uni.getStorageSync('token')
-	if (!hasLogin) {
-		// 是否是在白名单中的界面
-		if (witeList.indexOf(to.path) !== -1) {
-			next()
-		} else {
-			uni.showToast({
-				title:'请先登录, 3s后跳转至登录界面',
-				icon:'none'
-			})
-			setTimeout(() => {
-				uni.reLaunch({
-					url: '/pages/public/login'
-				})
-			}, 3000)
-			next(false)
-		}
-	} else {
-		next()
-	}
+	// if (process.env.NODE_ENV === 'development') { 
+	// 	console.log('to:' + to.path);
+	// 	console.log('from:' + from.path);
+	// }
+	// const hasLogin = uni.getStorageSync('token')
+	// if (!hasLogin) {
+	// 	// 是否是在白名单中的界面
+	// 	if (witeList.indexOf(to.path) !== -1) {
+	// 		next()
+	// 	} else {
+	// 		uni.showToast({
+	// 			title:'请先登录, 3s后跳转至登录界面',
+	// 			icon:'none'
+	// 		})
+	// 		setTimeout(() => {
+	// 			uni.reLaunch({
+	// 				url: '/pages/public/login'
+	// 			})
+	// 		}, 3000)
+	// 		next(false)
+	// 	}
+	// } else {
+	// 	next()
+	// }
+	next()
 });
 // 全局路由后置守卫
 router.afterEach((to, from) => {
