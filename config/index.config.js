24aabd993a27834c9945ad51a045f4f17cfd1868
yulf <yulf@startimes.com.cn>
2023-12-21 17:55:10 +0800

    完善房间预约界面

--- config/index.config.js
+++ config/index.config.js
@@ -3,13 +3,13 @@ const CONFIG = {
     development: {
         assetsPath: '/static', // 静态资源路径
         baseUrl: 'http://localhost:8500', // 后台接口请求地址
-        hostUrl: 'http://localhost:8600', // H5地址(前端运行地址)
+        // baseUrl: 'https://apply-room-record-83857-6-1322981911.sh.run.tcloudbase.com', // 后台接口请求地址
     },
     // 生产环境配置
     production: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: 'https://101.42.135.147:8500', // 后台接口请求地址
-        hostUrl: 'https://101.42.135.147:8500', // H5地址(前端运行地址)
+        // baseUrl: 'http://101.42.135.147:8500', // 后台接口请求地址
+        baseUrl: 'https://apply-room-record-83857-6-1322981911.sh.run.tcloudbase.com', // 后台接口请求地址
     }
 };
 export default CONFIG[process.env.NODE_ENV];
