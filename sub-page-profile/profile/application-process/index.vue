730b2be2a7b3817ddb125a9ba86954b15e54d05c
yulf <yulf@startimes.com.cn>
2024-01-12 13:23:59 +0800

    修改间隔背景色

--- sub-page-profile/profile/application-process/index.vue
+++ sub-page-profile/profile/application-process/index.vue
@@ -67,7 +67,7 @@
 						</view>
 					</view>
 				</view>
-				<view v-if="item.state !== 0" class="" style="background-color: #f2f2f2;width: 100%;padding: 2rpx;">
+				<view v-if="item.state !== 0" class="" style="background-color: #F8F7F8;width: 100%;padding: 2rpx;">
 
 				</view>
 				<view v-if="item.state !== 0"
